2681177a710012bf0aa7d0c41d9e2a666b6e44ff
“rename-z” <sachchidanandx@gmail.com>
2020-08-20 11:10:10 +0530

    recreating

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-import './widgets/new_transaction.dart';
-import './widgets/transaction_list.dart';
-import './models/transaction.dart';
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,116 +8,46 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Personal Expenses',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-        accentColor: Colors.lightGreen,
-        fontFamily: 'Quicksand',
-        textTheme: ThemeData.light().textTheme.copyWith(
-              headline6: TextStyle(
-                fontFamily: 'OpenSans',
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-        appBarTheme: AppBarTheme(
-          textTheme: ThemeData.light().textTheme.copyWith(
-                headline6: TextStyle(
-                  fontFamily: 'OpenSans',
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-        ),
-      ),
+      title: 'Flutter App',
       home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final List<Transaction> _userTransactions = [
-    // Transaction(
-    //   id: 't1',
-    //   title: 'Shoes',
-    //   amount: 86.56,
-    //   date: DateTime.now(),
-    // ),
-    // Transaction(
-    //   id: 't2',
-    //   title: 'Weekly Groceries',
-    //   amount: 16.35,
-    //   date: DateTime.now(),
-    // ),
-  ];
-
-  _addNewTransaction(String title, double amount) {
-    final newTransaction = Transaction(
-      title: title,
-      amount: amount,
+class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Banana',
+      amount: 24.25,
       date: DateTime.now(),
-      id: DateTime.now().toString(),
-    );
-
-    setState(() {
-      _userTransactions.add(newTransaction);
-    });
-  }
-
-  void _startAddNewTransaction(BuildContext context) {
-    showModalBottomSheet(
-      context: context,
-      builder: (_) {
-        return NewTransaction(_addNewTransaction);
-      },
-    );
-  }
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Apple',
+      amount: 14.25,
+      date: DateTime.now(),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'Personal Expenses',
-          // style: TextStyle(
-          //   fontFamily: 'Open Sans',
-          // ),
-        ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () {
-              _startAddNewTransaction(context);
-            },
-          ),
-        ],
+        title: Text('Flutter App'),
       ),
-      body: ListView(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Container(
-            width: double.infinity,
-            child: Card(
-              color: Colors.green,
-              child: Text('CHART!'),
-              elevation: 5,
-            ),
+          Card(
+            color: Colors.blue,
+            child: Text('CHART!'),
+            elevation: 5,
           ),
-          TransactionList(_userTransactions),
+          Card(
+            child: Text('LIST OF TX'),
+          ),
         ],
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        child: Icon(
-          Icons.add,
-        ),
-        onPressed: () {
-          _startAddNewTransaction(context);
-        },
       ),
     );
   }
